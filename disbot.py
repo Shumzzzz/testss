027e2cf64e8412afa04d0906e4dbdd52e86d9ca7
Shumzzzz <61364206+Shumzzzz@users.noreply.github.com>
2020-02-26 12:10:15 +0400

    Add files via upload

---
+++ disbot.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+Bot = commands.Bot(command_prefix='!')
+
+@Bot.event
+async def on_ready():
+    print('Бот онлайн!')
+
+@Bot.command(pass_context = True)
+async def hello(ctx):
+    await ctx.send("Hello!!!")
+
+Bot.run("NjgwODE5MzgxNjcyMDgzNTM2.XlV4Nw.b78xGe23RsCzcPmFMu0THQ9RRNk")
